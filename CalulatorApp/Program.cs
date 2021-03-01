6d28b14b130e662805c1d85c50dbf9924fdf13a1
Cordsenboyz <46309019+Cordsenboyz@users.noreply.github.com>
2021-03-01 10:45:03 +0100

    Add project files.

---
+++ CalulatorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalulatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
