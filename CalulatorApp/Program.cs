ff0c51a6b221480e5414001e88675726815c7654
Jacob <Jaco697r@elev.techcollege.dk>
2021-03-10 10:42:09 +0100

    [BUG] Was not a calculator

--- CalulatorApp/Program.cs
+++ CalulatorApp/Program.cs
@@ -1,76 +1,53 @@
 ﻿using System;
 
-namespace CalulatorApp
+namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            Console.WriteLine("Hello World!");
-=======
->>>>>>> parent of 34bda9d (Update Program.cs)
->>>>>>> tester
+            // Declare variables and then initialize to zero.
             int num1 = 0; int num2 = 0;
 
-            Console.WriteLine("BITCH");
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
 
+            // Ask the user to type the first number.
             Console.WriteLine("Type a number, and then press Enter");
             num1 = Convert.ToInt32(Console.ReadLine());
 
-                            ,--.
-                           { }
-                          K,   }
-                          /  ~Y`
-                     ,   /   /
-                    {_'-K.__/
-                      `/-.__L._
-                      /  ' /`\_}
-                     /  ' /
-             ____   /  ' /
-      ,-'~~~~    ~~/  ' /_
-    ,'             ``~~~  ',
-   (Y
-  {                         I
- {      -                    `,
- |       ',                   )
- |        |   ,..__ __.Y
- |    ., _./  Y ' / ^Y   J   )|
- \           |' /   |   |   ||
-  \          L_/    . _ (_,.'(
-   \,   ,      ^^""' / |      )
-     \_  \          /, L]     /
-       '-_~-,       ` `   ./`
-          `'{_            )
-              ^^\..___,.--`
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
 
+            // Ask the user to choose an option.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
 
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
                 case "d":
                     Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
                     break;
             }
-            Console.Write("Press any key to close the Calculator console app.................");
+            // Wait for the user to respond before closing.
+            Console.Write("Press any key to close the Calculator console app...");
             Console.ReadKey();
-<<<<<<< HEAD
-        }
-
-        static void TESTMERGE(string[] args)
-        {
-            int num1 = 0; int num2 = 0;
-
-=======
-<<<<<<< HEAD
-=======
-            Console.WriteLine("Hello World!");
->>>>>>> parent of 638153b (Update Program.cs)
-=======
->>>>>>> parent of f578b95 ([TEST])
->>>>>>> parent of 34bda9d (Update Program.cs)
->>>>>>> tester
         }
     }
+}
